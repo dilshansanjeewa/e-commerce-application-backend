70d341cf9b194175321fb67c796404d79e72f7fa
Dilshan Sanjeewa <dilshansanjeewads7@gmail.com>
2025-11-23 09:59:10 +0530

    Add database configuration

--- src/main/java/edu/icet/model/entity/Product.java
+++ src/main/java/edu/icet/model/entity/Product.java
@@ -1,5 +1,6 @@
 package edu.icet.model.entity;
 
+import jakarta.persistence.*;
 import lombok.*;
 
 @NoArgsConstructor
@@ -8,11 +9,17 @@ import lombok.*;
 @Getter
 @ToString
 
+@Entity
 public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String title;
     private String description;
     private String category;
     private double price;
+
+    @Column(name = "quantity")
     private int qty;
 }
